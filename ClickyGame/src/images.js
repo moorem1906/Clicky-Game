8c662957eef20506abcc34400ab37a4e6d221797
moorem1906 <moorem1906@gmail.com>
2019-12-09 18:15:19 -0500

    Rearranged Folder

---
+++ ClickyGame/src/images.js
@@ -0,0 +1,36 @@
+import BillRussel from './images/BillRussel.png';
+import Bird from './images/Bird.png';
+import EarlGoat from './images/EarlGoat.png';
+import Iverson from './images/Iverson.png';
+import JHarden from './images/JHarden.png';
+import KAJ from './images/KAJ.png';
+import KD from './images/KD.png';
+import Kobe from './images/Kobe.png';
+import LBJ from './images/LBJ.png';
+import MagicJ from './images/MagicJ.png';
+import MailMan from './images/MailMan.png';
+import MichaelJordan from './images/MichaelJordan.png';
+import Shaq from './images/Shaq.png';
+import Steph from './images/Steph.png';
+import WilkC from './images/WilkC.png';
+
+
+const images = [
+  BillRussel,
+  Bird,
+  EarlGoat,
+  Iverson,
+  JHarden,
+  KAJ,
+  KD,
+  Kobe,
+  LBJ,
+  MagicJ,
+  MailMan,
+  MichaelJordan,
+  Shaq,
+  Steph,
+  WilkC
+];
+
+export default images;
